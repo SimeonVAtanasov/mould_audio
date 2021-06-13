d264321be9ed9c0378b5da8768239484bfd22199
SimeonVAtanasov <simoatanasov76@gmail.com>
2021-06-13 23:01:17 +0300

    Updates

--- component---src-pages-about-js-93d786f4d46e282bfbb0.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[682],{5518:function(e,t,a){"use strict";a.r(t);var n=a(7294),l=a(5444),u=a(948),r=a(3751);t.default=function(){return n.createElement(u.Z,null,n.createElement(r.Z,{title:"About"}),n.createElement("h1",null,"That is going to be the about page"),n.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-about-js-93d786f4d46e282bfbb0.js.map
