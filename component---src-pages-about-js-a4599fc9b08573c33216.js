8ccb2736aca21034eb603edc2f5e6641b1604f17
Simeon Atanasov <simeon.atanasov@sirma.bg>
2021-06-23 17:01:06 +0300

    Updates

--- component---src-pages-about-js-a4599fc9b08573c33216.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkmould_audio=self.webpackChunkmould_audio||[]).push([[682],{5518:function(e,t,u){"use strict";u.r(t);var a=u(7294),n=u(5444),l=u(787),o=u(3751);t.default=function(){return a.createElement(l.Z,null,a.createElement(o.Z,{title:"About"}),a.createElement("h1",null,"That is going to be the about page"),a.createElement(n.Link,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-about-js-a4599fc9b08573c33216.js.map
