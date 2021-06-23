687b7376bcd54a49e6b381167d7ddf861ce0e023
Simeon Atanasov <simeon.atanasov@sirma.bg>
2021-06-23 16:56:31 +0300

    Updates

---
+++ component---src-pages-about-js-1ab912d5c28d1ecdd18a.js
@@ -0,0 +1,2 @@
+(self.webpackChunkmould_audio=self.webpackChunkmould_audio||[]).push([[682],{5518:function(e,t,u){"use strict";u.r(t);var a=u(7294),n=u(5444),l=u(6315),o=u(3751);t.default=function(){return a.createElement(l.Z,null,a.createElement(o.Z,{title:"About"}),a.createElement("h1",null,"That is going to be the about page"),a.createElement(n.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-about-js-1ab912d5c28d1ecdd18a.js.map
---
+++ component---src-pages-store-js-f0ceec64a4e512602216.js
@@ -0,0 +1,2 @@
+(self.webpackChunkmould_audio=self.webpackChunkmould_audio||[]).push([[500],{9721:function(e,t,n){"use strict";n.r(t);var a=n(7294),l=n(5444),o=n(6315),u=n(3751);t.default=function(){return a.createElement(o.Z,null,a.createElement(u.Z,{title:"Store"}),a.createElement("h1",null,"That is going to be the store page"),a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-store-js-f0ceec64a4e512602216.js.map
